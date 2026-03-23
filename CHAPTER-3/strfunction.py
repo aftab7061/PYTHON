5a0816c97fe4c9784bd645ab8914647c08d6b85d
Aftab Ansari <162883368+aftab7061@users.noreply.github.com>
2026-03-23 21:42:52 +0530

    Add files via upload

---
+++ CHAPTER-3/strfunction.py
@@ -0,0 +1,5 @@
+name="aftab"
+print(len(name))
+print(name.startswith("aft"))
+print(name.endswith("tab"))
+print(name.capitalize())
---
+++ CHAPTER-3/string.py
@@ -0,0 +1,8 @@
+name="aftab"
+print(len(name))
+# slice in python syntax name[s_index:l_index]
+print(name[0:3])
+print(name[-4:-1])
+print(name[:5])  # same as print(name[0:5])
+print(name[3:])  # same as print(name[3:4])
+
