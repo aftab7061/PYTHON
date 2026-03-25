9329f775f1936451c9364b24f3d5d9f5bc49d7ae
Aftab Ansari <162883368+aftab7061@users.noreply.github.com>
2026-03-25 22:17:32 +0530

    Add files via upload

---
+++ CHAPTER-4/tuples.py
@@ -0,0 +1,9 @@
+fruits=("apple","mango","orange",2,43,2,33,1,2,)
+print(type(fruits))
+print(len(fruits))
+print("-------- Tuple Methods ---------")
+co=fruits.count(2)
+print(co)
+
+# i=fruits.index(2)
+print(fruits.index(33))
