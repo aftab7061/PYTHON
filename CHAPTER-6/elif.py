b662e24569a3cb870a5058b133f96bfefefbab30
Aftab Ansari <162883368+aftab7061@users.noreply.github.com>
2026-03-27 23:07:52 +0530

    Add files via upload

---
+++ CHAPTER-6/elif.py
@@ -0,0 +1,12 @@
+a=int(input("enter your age :"))
+if(a>=18):
+    print("you are enter loops")
+    
+elif(a<0):
+    print("you are enter invalid negative age")
+    
+elif(a==0):
+    print("you are enter 0, is not  valid age")
+    
+# else:
+#     print("you are not enter loops")
---
+++ CHAPTER-6/ifcon.py
@@ -0,0 +1,6 @@
+a=int(input("enter your age :"))
+if(a>=18):
+    print("you are enter loops")
+else:
+    print("you are not enter loops")
+    
