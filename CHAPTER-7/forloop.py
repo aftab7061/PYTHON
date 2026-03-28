1940f18e59ca0addc8f42394530dbb2329ef6235
Aftab Ansari <162883368+aftab7061@users.noreply.github.com>
2026-03-28 20:36:05 +0530

    Add files via upload

---
+++ CHAPTER-7/forloop.py
@@ -0,0 +1,24 @@
+num=int(input("enter a number: "))
+for i in range(1,num):
+    print(i)
+
+#for loop with lists
+print("------------for loop with lists-------------")
+l=[2,53,222,12,56]
+print(l)
+for i in l:
+    print(i)
+
+#for loop with tuples
+print("-----------for loop with tuples---------")
+t=(23,2,"aftab",67,"sam")
+print(t)
+for i in t:
+    print(i)
+
+#for loop with string
+print("-----------for loop with string---------")
+s="aftab"
+print(s)
+for i in s:
+    print(i)
---
+++ CHAPTER-7/listloop.py
@@ -0,0 +1,5 @@
+l=[2,3 ,63,"aftab",True,0]
+i=0
+while(i<len(l)):
+    print(l[i])
+    i+=1
