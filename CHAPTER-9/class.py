bd4cbf17efe8842252df7e6a5279f83726ed464d
Aftab Ansari <162883368+aftab7061@users.noreply.github.com>
2026-05-01 17:11:08 +0530

    Add files via upload

---
+++ CHAPTER-9/class.py
@@ -0,0 +1,10 @@
+class First:
+  
+    def __init__(self):
+        self.name="aftab"
+        print(self.name)
+    # def show(self):
+    #     print(self.name)
+        
+obj=First()
+# obj.show()
---
+++ CHAPTER-9/class2.py
@@ -0,0 +1,10 @@
+class First:
+  
+    def __init__(self,name):
+        self.name=name
+        
+    def show(self):
+        print(self.name)
+        
+obj=First("aftab")
+obj.show()
